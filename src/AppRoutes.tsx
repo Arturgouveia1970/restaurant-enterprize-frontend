34477178d2b06d293d04954546174c0b76a3944d
Artur <artgouveiag@gmail.com>
2024-02-27 20:09:46 +0100

    manage restaurant up to image

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,22 +1,44 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserprofilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
-
+import ManageRestaurantPage from "./pages/manageRestaurantPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route element={<ProtectedRoute />}>
-        <Route path="/user-profile" element={<Layout><UserprofilePage /></Layout>} />
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserprofilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
       </Route>
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default AppRoutes;
